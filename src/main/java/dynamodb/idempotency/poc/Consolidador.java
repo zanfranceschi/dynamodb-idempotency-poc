2566311ca1ff6b7a87e90d018d4812c5b59cec8c
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2021-04-21 16:19:04 -0300

    Testes para paralelizar processamento do arquivo.

--- src/main/java/dynamodb/idempotency/poc/Consolidador.java
+++ src/main/java/dynamodb/idempotency/poc/Consolidador.java
@@ -1,104 +1,88 @@
 package dynamodb.idempotency.poc;
 
-import io.micronaut.context.event.StartupEvent;
-import io.micronaut.runtime.event.annotation.EventListener;
+
+import io.micronaut.context.annotation.Prototype;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Singleton;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.net.URI;
 import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
+
 import software.amazon.awssdk.services.dynamodb.model.*;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
-
-@Singleton
+@Prototype
 public class Consolidador {
 
     private static final Logger logger = LoggerFactory.getLogger(Consolidador.class);
     private DynamoDbClient dynamoDbClient;
+    private final DistribuidorConsolidacao distribuidorConsolidacao;
 
-    public Consolidador(DynamoDbClientBuilder dynamoDbClientBuilder) {
-        dynamoDbClient = dynamoDbClientBuilder
+    public Consolidador(DistribuidorConsolidacao distribuidorConsolidacao) {
+
+        this.distribuidorConsolidacao = distribuidorConsolidacao;
+
+        dynamoDbClient = DynamoDbClient.builder()
                 // Usar o DynamoDB localmente
                 // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.DownloadingAndRunning.html
                 .endpointOverride(URI.create("http://localhost:8000"))
                 .build();
-        ListTablesResponse response = dynamoDbClient.listTables();
-
-        for (String table: response.tableNames()) {
-            logger.info(String.format("table: %s", table));
-        }
     }
 
-    @EventListener
-    public void run(StartupEvent e) {
-        try {
-            String arquivoSaldosCaminho = getClass().getClassLoader().getResource("saldos.txt").getPath(); // que rolê pra achar um caminho, hein?!
-            FileInputStream fis = new FileInputStream(arquivoSaldosCaminho);
+    @SneakyThrows
+    public void iniciar() {
 
-            Scanner sc = new Scanner(fis);
+        ConcurrentLinkedQueue<String> queue = distribuidorConsolidacao.getQueue();
 
-            List<UtilizacaoSaldo> utilizacoes = new ArrayList<>();
+        while (true) {
 
-            logger.info("data    | nome           | saldoUtilizado");
-            logger.info("--------+----------------+----------------");
+            String linha = queue.poll();
 
-            while (sc.hasNextLine()) {
-                String linha = sc.nextLine();
-
-
-                String transacaoId = linha.substring(0, 2).trim();
-                String cliente = linha.substring(10, 24).trim();
-                String data = linha.substring(2, 9).trim();
-                String saldoUtilizado = linha.substring(25, 29).trim();
-
-                UtilizacaoSaldo utilizacaoSaldo = new UtilizacaoSaldo(transacaoId, cliente, data, saldoUtilizado);
-
-                logger.info(String.format("%s | %s | %s", data, cliente, saldoUtilizado));
-
-                Map<String, AttributeValue> item = new HashMap<>();
-                item.put("transacaoId", AttributeValue.builder().s(utilizacaoSaldo.getTransacaoId()).build());
-                item.put("cliente", AttributeValue.builder().s(utilizacaoSaldo.getCliente()).build());
-                item.put("data", AttributeValue.builder().s(utilizacaoSaldo.getData()).build());
-                item.put("valorUtilizado", AttributeValue.builder().s(utilizacaoSaldo.getValorUtilizado()).build());
-                item.put("hashCode", AttributeValue.builder().n(String.valueOf(utilizacaoSaldo.hashCode())).build());
-
-                Map<String, AttributeValue> attrValues = new HashMap<>();
-                attrValues.put(":hashCode", AttributeValue.builder().n(String.valueOf(utilizacaoSaldo.hashCode())).build());
-
-                try {
-                    PutItemResponse response = dynamoDbClient.putItem(
-                            PutItemRequest.builder()
-                                    .tableName("UtilizacaoSaldo")
-                                    .item(item)
-                                    .returnValues("ALL_OLD")
-                                    .expressionAttributeValues(attrValues)
-                                    .conditionExpression("hashCode <> :hashCode")
-                                    .build());
-
-                    if (response.attributes().size() == 0) {
-                        logger.info("registro criado");
-                    } else {
-                        logger.info("registro atualziado");
-                    }
-                }
-                catch (ConditionalCheckFailedException cex) {
-                    logger.info("registro idêntico existente: ignorado!");
-                }
+            if (linha == null) {
+                Thread.sleep(100);
+                continue;
             }
 
-            logger.info("--------+----------------+----------------");
+            String transacaoId = linha.substring(0, 2).trim();
+            String cliente = linha.substring(10, 24).trim();
+            String data = linha.substring(2, 9).trim();
+            String saldoUtilizado = linha.substring(25, 29).trim();
 
-            sc.close();
+            UtilizacaoSaldo utilizacaoSaldo = new UtilizacaoSaldo(transacaoId, cliente, data, saldoUtilizado);
 
-        } catch (IOException ex) {
-            logger.error("erro ao ler o arquivo", ex);
+            Map<String, AttributeValue> item = new HashMap<>();
+            item.put("transacaoId", AttributeValue.builder().s(utilizacaoSaldo.getTransacaoId()).build());
+            item.put("cliente", AttributeValue.builder().s(utilizacaoSaldo.getCliente()).build());
+            item.put("data", AttributeValue.builder().s(utilizacaoSaldo.getData()).build());
+            item.put("valorUtilizado", AttributeValue.builder().s(utilizacaoSaldo.getValorUtilizado()).build());
+            item.put("hashCode", AttributeValue.builder().n(String.valueOf(utilizacaoSaldo.hashCode())).build());
+
+            Map<String, AttributeValue> attrValues = new HashMap<>();
+
+            attrValues.put(":hashCode", AttributeValue.builder().n(String.valueOf(utilizacaoSaldo.hashCode())).build());
+
+            try {
+                PutItemResponse response = dynamoDbClient.putItem(
+                        PutItemRequest.builder()
+                                .tableName("UtilizacaoSaldo")
+                                .item(item)
+                                .returnValues("ALL_OLD")
+                                .expressionAttributeValues(attrValues)
+                                .conditionExpression("hashCode <> :hashCode")
+                                .build());
+
+                if (response.attributes().size() == 0) {
+                    logger.info("registro criado: " + linha);
+                } else {
+                    logger.info("registro atualziado: " + linha);
+                }
+            }
+            catch (ConditionalCheckFailedException cex) {
+                logger.info("registro idêntico existente: ignorado!: " + linha);
+            }
         }
     }
-
 }
