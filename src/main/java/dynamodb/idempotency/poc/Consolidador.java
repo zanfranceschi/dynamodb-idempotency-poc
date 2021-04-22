5a817bb19799dea4736480da2e9e0fb6e1e76532
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2021-04-21 21:09:54 -0300

    ..

--- src/main/java/dynamodb/idempotency/poc/Consolidador.java
+++ src/main/java/dynamodb/idempotency/poc/Consolidador.java
@@ -1,29 +1,38 @@
 package dynamodb.idempotency.poc;
 
-
-import io.micronaut.context.annotation.Prototype;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
 import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
 import java.net.URI;
 import java.util.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
 
 import software.amazon.awssdk.services.dynamodb.model.*;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
-@Prototype
+import javax.inject.Singleton;
+
+@Singleton
 public class Consolidador {
 
     private static final Logger logger = LoggerFactory.getLogger(Consolidador.class);
+
     private DynamoDbClient dynamoDbClient;
-    private final DistribuidorConsolidacao distribuidorConsolidacao;
 
-    public Consolidador(DistribuidorConsolidacao distribuidorConsolidacao) {
+    public Consolidador() {
+        /*
+             Para criar uma tabela usando o DynamoDB localmente com o aws cli:
 
-        this.distribuidorConsolidacao = distribuidorConsolidacao;
+             aws dynamodb create-table \
+                --table-name UtilizacaoSaldo \
+                --attribute-definitions AttributeName=transacaoId,AttributeType=S \
+                --key-schema AttributeName=transacaoId,KeyType=HASH \
+                --provisioned-throughput ReadCapacityUnits=100,WriteCapacityUnits=100 \
+                --endpoint-url http://localhost:8000
+        */
 
         dynamoDbClient = DynamoDbClient.builder()
                 // Usar o DynamoDB localmente
@@ -33,26 +42,30 @@ public class Consolidador {
     }
 
     @SneakyThrows
-    public void iniciar() {
+    @EventListener
+    public void iniciar(StartupEvent e) {
 
-        ConcurrentLinkedQueue<String> queue = distribuidorConsolidacao.getQueue();
+        String arquivoSaldosCaminho = getClass().getClassLoader().getResource("saldos.txt").getPath();
+        FileInputStream fis = new FileInputStream(arquivoSaldosCaminho);
 
-        while (true) {
+        Scanner sc = new Scanner(fis);
 
-            String linha = queue.poll();
+        while (sc.hasNextLine()) {
 
-            if (linha == null) {
-                Thread.sleep(100);
-                continue;
-            }
+            logger.info(gerarLinhaMock());
 
-            String transacaoId = linha.substring(0, 2).trim();
-            String cliente = linha.substring(10, 24).trim();
-            String data = linha.substring(2, 9).trim();
-            String saldoUtilizado = linha.substring(25, 29).trim();
+            String linha = sc.nextLine();
 
+            // Arquivo posicional
+            String transacaoId = linha.substring(0, 3).trim();
+            String data = linha.substring(3, 10).trim();
+            String cliente = linha.substring(11, 25).trim();
+            String saldoUtilizado = linha.substring(26, 30).trim();
+
+            // Instancia o objeto para poder obter o hashCode
             UtilizacaoSaldo utilizacaoSaldo = new UtilizacaoSaldo(transacaoId, cliente, data, saldoUtilizado);
 
+            // POJO para Registro DynamoDB
             Map<String, AttributeValue> item = new HashMap<>();
             item.put("transacaoId", AttributeValue.builder().s(utilizacaoSaldo.getTransacaoId()).build());
             item.put("cliente", AttributeValue.builder().s(utilizacaoSaldo.getCliente()).build());
@@ -60,8 +73,8 @@ public class Consolidador {
             item.put("valorUtilizado", AttributeValue.builder().s(utilizacaoSaldo.getValorUtilizado()).build());
             item.put("hashCode", AttributeValue.builder().n(String.valueOf(utilizacaoSaldo.hashCode())).build());
 
+            // Parâmetro para a Condition Expression (hashCode <> objeto.hashCode())
             Map<String, AttributeValue> attrValues = new HashMap<>();
-
             attrValues.put(":hashCode", AttributeValue.builder().n(String.valueOf(utilizacaoSaldo.hashCode())).build());
 
             try {
@@ -69,9 +82,11 @@ public class Consolidador {
                         PutItemRequest.builder()
                                 .tableName("UtilizacaoSaldo")
                                 .item(item)
+                                // ALL_OLD faz com que atualizações retornem os valores anteriores
                                 .returnValues("ALL_OLD")
-                                .expressionAttributeValues(attrValues)
+                                // se a condição falhar, uma ConditionalCheckFailedException é lançada
                                 .conditionExpression("hashCode <> :hashCode")
+                                .expressionAttributeValues(attrValues)
                                 .build());
 
                 if (response.attributes().size() == 0) {
@@ -79,10 +94,17 @@ public class Consolidador {
                 } else {
                     logger.info("registro atualziado: " + linha);
                 }
-            }
-            catch (ConditionalCheckFailedException cex) {
+            } catch (ConditionalCheckFailedException cex) {
                 logger.info("registro idêntico existente: ignorado!: " + linha);
             }
         }
+    }
+
+    public static String gerarLinhaMock() {
+        Random rand = new Random();
+        String transacaoId = String.format("%03d", rand.nextInt(999));
+        String data = String.format("%08d", rand.nextInt(99999999));
+
+        return transacaoId + " " + data;
     }
 }
